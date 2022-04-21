775d2068b0b3c866de3c60c7ac9c962c2c4bd5a6
msjun23 <msjun23@gmail.com>
2022-04-21 23:13:02 +0900

    Update wall follower ver1.

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -12,33 +12,14 @@ class node:
         
         self.res = 0.391            # RPLiDAR S1 Resolution in degree
         self.mode = 0
+        self.cnt = 0
         self.kp = 1
         self.kd = 0.001
+        self.dist_ref = 0.5         # Reference distance between wall and robot
         self.prev_err = 0.0
-        self.speed = 0.1
-        self.turn = -1.0
-
+        
         self.cmd_vel = Twist()
         self.pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        
-    def ScanSubscriber(self, data):
-        ranges = data.ranges
-
-        # LiDAR range
-        #            460(180)
-        #               x
-        # 690(270)   y--|     230(90)
-        #
-        #       920(360)|0(0)
-        # degree: 150~180
-        # deg_s_point = int(120 / self.res)
-        # deg_e_point = int(240 / self.res)
-        # deg_s_point = int(60 / self.res)
-        # deg_e_point = int(120 / self.res)
-        # ranges = ranges[deg_s_point:deg_e_point]    # Front right scan value
-        #rospy.loginfo(ranges)
-        
-        self.WallFollow(ranges)
         
     def Deg2Idx(self, deg):
         return int(deg / self.res)
@@ -73,29 +54,68 @@ class node:
             
         self.pub_vel.publish(self.cmd_vel)
         
+    def Assigned(self, ranges):
+        res = 60.0 / len(ranges)
+        
+        x_coord = []
+        for i in range(len(ranges)):
+            x_coord.append(ranges[i] * np.cos((-30.0 + res * i) / 180 * np.pi))
+            
+        x_std = np.std(x_coord)
+        if (x_std < 0.01):
+            return True
+        return False
+        
     def Rotating(self):
         # Rotate until attach right side of robot to the wall
         self.cmd_vel.linear.x = 0.0
         self.cmd_vel.linear.y = 0.0
-        self.cmd_vel.angular.z = 0.5
+        self.cmd_vel.angular.z = 1.0
             
-        rospy.loginfo('Rotating...')
+        rospy.loginfo('Rotating... ' + str(self.cnt))
         self.pub_vel.publish(self.cmd_vel)
         
-    def Driving(self):
+    def Driving(self, ranges):
         # Follow the wall
-        self.cmd_vel.linear.x = 0.5
-        self.cmd_vel.angular.z = 0.0
+        
+        b = ranges[self.Deg2Idx(90)]
+        a = ranges[self.Deg2Idx(150)]
+        theta = 60.0 / 180.0 * np.pi
+        alpha = np.arctan((a*np.cos(theta) - b) / (a*np.sin(theta)))
+        #print(alpha / np.pi * 180.0)
+        
+        Dt = b*np.cos(alpha)
+        #print(Dt)
+        
+        L = 0.05        # Distance that robot drive to straightly when linear speed is 0.5
+        Dt1 = Dt + L*np.sin(alpha)
+        #print(Dt1)
+        
+        speed = 0.5
+        dist_err = self.dist_ref - Dt1
+        turn = self.kp * dist_err + self.kd * (dist_err - self.prev_err)
+        self.prev_err = dist_err
+        
+        self.cmd_vel.linear.x = speed
+        self.cmd_vel.linear.y = 0.0
+        self.cmd_vel.angular.z = turn
         
         rospy.loginfo('Following the wall')
         self.pub_vel.publish(self.cmd_vel)
-    
-    def WallFollow(self, ranges):
+        
+    def ScanSubscriber(self, data):
+        # LiDAR range
+        #            460(180)
+        #               x
+        # 690(270)   y--|     230(90)
+        #
+        #       920(360)|0(0)
+        # 34~41 / 139~146 / 214~221 / 318~326 degree is blocked by robot frame
+        # for i in range(360):
+        #     print(i, ranges[self.Deg2Idx(i)])
+        ranges = data.ranges
+        
         if (self.mode == 0):
-            # ranges: front right ~ front
-            # Filtering LiDAR data blocked by robot frame
-            #ranges = [dist for dist in ranges if dist > 0.3]
-                    
             # Find nearest wall
             dist_r = dist_r = sum(ranges[self.Deg2Idx(60):self.Deg2Idx(120)]) \
                             / len(ranges[self.Deg2Idx(60):self.Deg2Idx(120)])       # Avarage of right side scan data
@@ -108,26 +128,24 @@ class node:
             dist = [dist_r, dist_f, dist_l, dist_b]
             nearest_dir = dist.index(min(dist))     # 0: right, 1: front, 2: left, 3: back
             
-            dist_ref = 1.0                          # Reference distance between wall and robot
-            dist_err = dist_ref - dist[nearest_dir]
+            dist_err = self.dist_ref - dist[nearest_dir]
             
             if (abs(dist_err) > 0.1):
                 # Approaching to the wall is first
                 self.ToTheWall(nearest_dir, dist_err)
             else:
-                # if (nearest_dir == 0):              # Right side of robot is attached to the wall
-                #     self.Driving()
-                # else:                               # Other side of robot is attached to the wall
-                if (abs(ranges[self.Deg2Idx(90)] - 1.0) < 0.1):
-                    # Right side of robot is attached to the wall
-                    self.Driving()
+                if (nearest_dir == 0):
+                    self.Driving(ranges)
                 else:
                     self.mode = 1
-        else:
-            if (abs(ranges[self.Deg2Idx(90)] - 1.0) < 0.1):
+        elif (self.mode == 1):
+            if (self.Assigned(ranges[self.Deg2Idx(60):self.Deg2Idx(120)]) and self.cnt >= 50):
                 # Right side of robot is attached to the wall
                 self.mode = 0
+                self.cnt = 0
             else:
+                # Other side of robot is attached to the wall
+                self.cnt += 1
                 self.Rotating()
         
     def QuitHandler(self):
